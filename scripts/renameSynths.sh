17c9dba85efcfa985b35613e1a2b5c983abcdc87
Emilio Rojas <emilio93@gmail.com>
2018-10-15 01:30:35 -0600

    agregar makefile y scripts requeridos
    makefile se debe revisar más cuidadosamente, se tomo de otro proyecto y se adaptó rápidamente

---
+++ scripts/renameSynths.sh
@@ -0,0 +1,14 @@
+folder=build
+# folder=$1
+
+
+cd $folder
+for file in ./*-sintetizado.v; do
+  moduleName=${file/.\//}
+  moduleName=${moduleName/-sintetizado.v/}
+  newModuleName=${moduleName}Synth
+  echo "build/${file/.\//}: ${moduleName} -> ${newModuleName}"
+  echo "sed -i \"s/module ${moduleName}/module  ${newModuleName}/g\" \"${file}\""
+  sed -i "s/module ${moduleName}/module  ${newModuleName}/" "${file}"
+  echo ""
+done
