59f971521d643d817b0c93c796de401d1707eb21
Emilio Rojas <emilio93@gmail.com>
2018-10-23 03:36:38 -0600

    habilitar salida de archivos dot

--- scripts/yosys.tcl
+++ scripts/yosys.tcl
@@ -8,42 +8,42 @@ set cellLib        osu018_stdcells
 yosys read_verilog -sv $vlogFileName
 
 hierarchy -check -top $vlogModuleName
-# show -prefix pdfs/$vlogModuleName-original -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-original -colors 3 -viewer echo $vlogModuleName
 
 yosys proc
-# show -prefix pdfs/$vlogModuleName-proc -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-proc -colors 3 -viewer echo $vlogModuleName
 
 opt
-# show -prefix pdfs/$vlogModuleName-proc_opt -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-proc_opt -colors 3 -viewer echo $vlogModuleName
 
 fsm
-# show -prefix pdfs/$vlogModuleName-fsm -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-fsm -colors 3 -viewer echo $vlogModuleName
 
 opt
-# show -prefix pdfs/$vlogModuleName-fsm_opt -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-fsm_opt -colors 3 -viewer echo $vlogModuleName
 
 memory
-# show -prefix pdfs/$vlogModuleName-memory -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-memory -colors 3 -viewer echo $vlogModuleName
 
 opt
-# show -prefix pdfs/$vlogModuleName-memory_opt -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-memory_opt -colors 3 -viewer echo $vlogModuleName
 
 techmap
-# show -prefix pdfs/$vlogModuleName-techmap -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-techmap -colors 3 -viewer echo $vlogModuleName
 
 opt
-# show -prefix pdfs/$vlogModuleName-techmap_opt -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-techmap_opt -colors 3 -viewer echo $vlogModuleName
 
 write_verilog ./build/$vlogModuleName-rtlil.v
 
 dfflibmap -liberty ./lib/$cellLib.lib
 
-# show -prefix pdfs/$vlogModuleName-dff_seq -lib ./lib/$cellLib.v -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-dff_seq -lib ./lib/$cellLib.v -colors 3 -viewer echo $vlogModuleName
 
 abc -liberty ./lib/$cellLib.lib
-# show -prefix pdfs/$vlogModuleName-abc_comb -lib ./lib/$cellLib.v -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-abc_comb -lib ./lib/$cellLib.v -colors 3 -viewer echo $vlogModuleName
 
 clean
 
-# show -prefix pdfs/$vlogModuleName-synth -lib ./lib/$cellLib.v -viewer echo -colors 3 -viewer echo $vlogModuleName
+show -prefix pdfs/$vlogModuleName-synth -lib ./lib/$cellLib.v -viewer echo -colors 3 -viewer echo $vlogModuleName
 write_verilog ./build/$vlogModuleName-sintetizado.v
