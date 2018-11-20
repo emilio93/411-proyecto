d1599b0e374af6f609f4a674b331fffd11fde686
duniab <s4sibarahona@gmail.com>
2018-11-19 22:58:02 -0600

    qflow

---
+++ Qflow/project_vars.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/tcsh -f
+#------------------------------------------------------------
+# project variables for project /home/du/Files_du/UCR/II_18/MicroE/PROYECTO/QFLOW
+#------------------------------------------------------------
+
+# Synthesis command options:
+# -------------------------------------------
+# set yosys_options = 
+# set yosys_script = 
+# set yosys_debug = 
+# set abc_script = 
+# set nobuffers = 
+# set nofanout = 
+# set fanout_options = 
+
+# Placement command options:
+# -------------------------------------------
+# set initial_density = 
+# set graywolf_options = 
+# set addspacers_options = "-stripe 5 200 PG -nostretch"
+
+# Router command options:
+# -------------------------------------------
+# set route_layers = 
+# set via_stacks = 
+# set qrouter_options = 
+
+# Minimum operating period of the clock (in ps)
+# set vesta_options = "--period 1E5"
+
+#------------------------------------------------------------
+
+set initial_density = 0.8
