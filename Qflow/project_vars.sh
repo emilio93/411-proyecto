d737443adf4409e07a74436f5e0f3dca95b9989e
mlndz28 <fabian.melendez.a@gmail.com>
2018-12-06 17:20:36 -0600

    Qflow folder cleaned
    Deleted duplicated files and added generated files and folders to .git ignore.

--- Qflow/project_vars.sh
+++ Qflow/project_vars.sh
@@ -5,29 +5,29 @@
 
 # Synthesis command options:
 # -------------------------------------------
-# set yosys_options = 
-# set yosys_script = 
-# set yosys_debug = 
-# set abc_script = 
-# set nobuffers = 
-# set nofanout = 
-# set fanout_options = 
+# set yosys_options =
+# set yosys_script =
+# set yosys_debug =
+# set abc_script =
+# set nobuffers =
+# set nofanout =
+# set fanout_options =
 
 # Placement command options:
 # -------------------------------------------
-# set initial_density = 
-# set graywolf_options = 
+# set initial_density =
+# set graywolf_options =
 # set addspacers_options = "-stripe 5 200 PG -nostretch"
 
 # Router command options:
 # -------------------------------------------
-# set route_layers = 
-# set via_stacks = 
-# set qrouter_options = 
+# set route_layers =
+# set via_stacks =
+# set qrouter_options =
 
 # Minimum operating period of the clock (in ps)
 # set vesta_options = "--period 1E5"
 
 #------------------------------------------------------------
 
-set initial_density = 0.8
+set initial_density = 0.6
